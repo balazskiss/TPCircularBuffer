768ce5121abbdec0f549152fc755625a6e9100d9
Balázs Kiss <dev@balazskiss.name>
2018-10-09 11:33:19 +0200

    added project

---
+++ TPCircularBuffer/TPCircularBuffer/TPCircularBuffer.h
@@ -0,0 +1,19 @@
+//
+//  TPCircularBuffer.h
+//  TPCircularBuffer
+//
+//  Created by Balázs Kiss on 2018. 10. 09..
+//  Copyright © 2018. Synervoz Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TPCircularBuffer.
+FOUNDATION_EXPORT double TPCircularBufferVersionNumber;
+
+//! Project version string for TPCircularBuffer.
+FOUNDATION_EXPORT const unsigned char TPCircularBufferVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TPCircularBuffer/PublicHeader.h>
+
+
